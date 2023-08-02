44b45bb544c1c05bffa6b0a134ac3ee683b94f27
Luigi Pedace <luigipedace@icloud.com>
2023-08-02 18:46:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById('showForm').addEventListener('click', function() {
+    document.getElementById('emailForm').style.display = 'block';
+    document.getElementById('showForm').style.display = 'none';
+});
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var messageElement = document.getElementById('message');
+    messageElement.textContent = "Thank you for your interest. We'll be in touch!";
+    messageElement.style.display = 'block';
+    document.getElementById('submitButton').style.display = 'none';
+    document.getElementById('email').style.display = 'none';
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('header').style.display = 'none';
+    document.getElementById('successTick').style.display = 'inline';
+});
