163a37b053716e2a87555b74705f629a996f2e4b
Luigi Pedace <luigipedace@icloud.com>
2023-08-07 14:30:14 +0200

    Fix

--- main.js
+++ main.js
@@ -4,12 +4,25 @@ document.getElementById('showForm').addEventListener('click', function() {
 });
 
 document.getElementById('emailForm').addEventListener('submit', function(event) {
-    var messageElement = document.getElementById('message');
-    messageElement.textContent = "Thank you for your interest. We'll be in touch!";
-    messageElement.style.display = 'block';
-    document.getElementById('submitButton').style.display = 'none';
-    document.getElementById('email').style.display = 'none';
-    document.getElementById('logo').style.display = 'none';
-    document.getElementById('header').style.display = 'none';
-    document.getElementById('successTick').style.display = 'inline';
+    event.preventDefault();
+    var formData = new FormData(this);
+    fetch('https://script.google.com/macros/s/AKfycbyj3mtyQU0AzQKz2cZ1vDuP1AhQ7BdHpI6YCHIxtZhnIbyDMcZOU_uRprU-NVks2V5s/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        if (data === 'Success') {
+            var messageElement = document.getElementById('message');
+            messageElement.textContent = "Thank you for your interest. We'll be in touch!";
+            messageElement.style.display = 'block';
+            document.getElementById('submitButton').style.display = 'none';
+            document.getElementById('email').style.display = 'none';
+            document.getElementById('logo').style.display = 'none';
+            document.getElementById('header').style.display = 'none';
+            document.getElementById('successTick').style.display = 'inline';
+        } else {
+            // handle error, maybe show a different message
+        }
+    });
 });
